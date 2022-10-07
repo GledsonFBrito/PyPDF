982df5e1896a05ad3617b10d9b1d6f30a19f7ef5
Gledson Ferreira <gledsonferreira860@gmail.com>
2022-10-07 15:52:04 -0300

    Compactar e descompactar arquivos com Python

--- zipFile.py
+++ zipFile.py
@@ -5,18 +5,18 @@ import os
 # Para caminhos com barra invertida (\), utilize r (normalmente no windows)
 caminho = r'C:\Users\Gledson\Documents\python_test'
 
-# ESCREVE
+# ESCREVE:
 with ZipFile('arquivo.zip', 'w') as zip:  # cria um arquivo zip
     for arquivo in os.listdir(caminho):  # arquivos da pasta (não contando com subpastas)
         caminho_completo = os.path.join(caminho, arquivo)  # se você quiser incluir as demais pastas, 
         zip.write(caminho_completo, arquivo)               # use os.walk()
         
 
-# LISTA ARQUIVOS
+# LISTA ARQUIVOS:
 with ZipFile('arquivo.zip', 'r') as zip:
     for arquivo in zip.namelist():
         print(arquivo)
 
-# EXTRAI
+# EXTRAI:
 with ZipFile('arquivo.zip', 'r') as zip:
     zip.extractall('descompactado') 
